5f88583d2efcc1a83b96d2f155f7d0adc0e65013
Dan Lee <dan-lee1@hotmail.co.uk>
2022-06-03 15:33:06 +0100

    remove old darkmode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,6 @@
 import MainFeed from './components/mainFeed';
-// import {ThemeProvider} from "styled-components";
-// import { GlobalStyles } from "./components/globalStyles";
-// import { lightTheme, darkTheme } from "./components/Theme"
 // import { useState} from "react";
 function App() {
-  // const [theme, setTheme] = useState('light');
-  // const themeToggler = () => {
-  //   theme === 'light' ? setTheme('dark') : setTheme('light')
-// }
   return (
     <div className="App">
       <header className="App-header">
