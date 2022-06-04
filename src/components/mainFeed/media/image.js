a69e2394f3c73462caec89c30c23f75b85d1de16
Dan Lee <dan-lee1@hotmail.co.uk>
2022-06-04 15:48:11 +0100

    media viewer

--- src/components/mainFeed/media/image.js
+++ src/components/mainFeed/media/image.js
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react';
 import styles from './post.module.css';
-import PostBanner from './postBanner';
-import PostFooter from './postFooter';
 function ImageMedia(props){
     let [useZoomed, setZoomed] = useState(false);
     let [useImageZoomed, setImageZoomed] = useState(<div></div>);
@@ -16,7 +14,6 @@ function ImageMedia(props){
     useEffect( () => {
         if(props.image !== null){
             setImage(<div onClick={makeImgBig} className={styles.imgArea}><img className={styles.imgStandard} src={"https://cdn.danlee.uk/content/posts/"+props.image} alt={props.image}/></div>)
-            setFooter(<PostFooter location={props.image}/>)
             if(useZoomed === true){
                 document.body.style.overflow = 'hidden'
                 setImageZoomed(<div onClick={makeImgBig} className={styles.imageZoomedBG}><span className={styles.date}>{props.date}</span><span className={styles.close}>&times;</span><img className={styles.imageZoomed} src={"https://cdn.danlee.uk/content/posts/"+props.image} alt={props.image}/></div>)
@@ -26,9 +23,7 @@ function ImageMedia(props){
                 setImageZoomed(<div></div>)
             }
         }
-        else{
-            setFooter(<PostFooter location={null}/>)
-        }
+
     }, [useZoomed])
     
     return (
