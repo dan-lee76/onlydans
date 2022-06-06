d7a4f021dec65e94b4e49e1e19067323af9267f2
Dan Lee <dan-lee1@hotmail.co.uk>
2022-06-06 19:44:43 +0100

    goes to next image

--- src/components/mainFeed/media/image.js
+++ src/components/mainFeed/media/image.js
@@ -4,30 +4,37 @@ function ImageMedia(props){
     let [useZoomed, setZoomed] = useState(false);
     let [useImageZoomed, setImageZoomed] = useState(<div></div>);
     let [useImage, setImage] = useState(<div></div>);
-    let [useImageData, setImageData] = useState(props.postData[props.postData.findIndex(function(item, i){
+    let [useCurrentIndex, setCurrentIndex] = useState(props.postData.findIndex(function(item, i){
         return parseInt(item.id) === props.id
-      })])
+      }))
+    let [useImageData, setImageData] = useState(props.postData[useCurrentIndex])
 
     function makeImgBig(){
-        setImageData(props.postData[props.postData.findIndex(function(item, i){
+        setCurrentIndex(props.postData.findIndex(function(item, i){
             return parseInt(item.id) === props.id
-          })])
+          }))
+        setImageData(props.postData[useCurrentIndex])
         setZoomed(!useZoomed);
         
     }
 
     function nextImage(){
         console.log(props.id)
-
-        let indexOfCurrent = props.postData.findIndex(function(item, i){
-            return parseInt(item.id) === props.id
-          });
-        let idAdd = indexOfCurrent + 1
-        console.log(indexOfCurrent)
-        // setImageLoc(props.postData[indexOfCurrent+1].location)
-        console.log(props.postData[indexOfCurrent+1])
-        console.log(props.postData[indexOfCurrent])
-        setImageData(props.postData[indexOfCurrent+1])
+        // let idAdd = indexOfCurrent + 1
+        // console.log(indexOfCurrent)
+        // // setImageLoc(props.postData[indexOfCurrent+1].location)
+        // console.log(props.postData[indexOfCurrent+1])
+        // console.log(props.postData[indexOfCurrent])
+        // setImageData(props.postData[indexOfCurrent+1])
+        let newIndex = useCurrentIndex
+        do{
+            newIndex++
+            setCurrentIndex(newIndex)
+            setImageData(props.postData[newIndex])
+        }while(props.postData[newIndex].location == null)
+        // setCurrentIndex(newIndex)
+        // setImageData(props.postData[newIndex])
+        console.log(props.postData[newIndex].location)
     }
 
 
@@ -37,12 +44,12 @@ function ImageMedia(props){
             if(useZoomed === true){
                 document.body.style.overflow = 'hidden'
                 setImageZoomed(<div className={styles.imageZoomedBG}>
-                    <span className={styles.date}>{props.date}</span>
+                    <span className={styles.date}>{useImageData.date}</span>
                     <span onClick={makeImgBig} className={styles.close}>&times;</span>
                     <img className={styles.imageZoomed} src={"https://cdn.danlee.uk/content/posts/"+useImageData.location} alt={props.image}/>
                     <span onClick={nextImage} className={styles.nextImage}>Next</span>
                     <span onClick={nextImage} className={styles.previousImage}>Prev</span>
-                    <span className={styles.content}>{props.content}</span>
+                    <span className={styles.content}>{useImageData.description}</span>
                     </div>)
             }
             else{
