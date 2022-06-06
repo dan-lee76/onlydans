5a09c52fdcda8c6dde86b1bdda015fb7135f7e73
Dan Lee <dan-lee1@hotmail.co.uk>
2022-06-06 18:41:46 +0100

    close button close

--- src/components/mainFeed/media/image.js
+++ src/components/mainFeed/media/image.js
@@ -16,9 +16,9 @@ function ImageMedia(props){
             setImage(<div onClick={makeImgBig} className={styles.imgArea}><img className={styles.imgStandard} src={"https://cdn.danlee.uk/content/posts/"+props.image} alt={props.image}/></div>)
             if(useZoomed === true){
                 document.body.style.overflow = 'hidden'
-                setImageZoomed(<div onClick={makeImgBig} className={styles.imageZoomedBG}>
+                setImageZoomed(<div className={styles.imageZoomedBG}>
                     <span className={styles.date}>{props.date}</span>
-                    <span className={styles.close}>&times;</span>
+                    <span onClick={makeImgBig} className={styles.close}>&times;</span>
                     <img className={styles.imageZoomed} src={"https://cdn.danlee.uk/content/posts/"+props.image} alt={props.image}/>
                     <span className={styles.content}>{props.content}</span>
                     </div>)
