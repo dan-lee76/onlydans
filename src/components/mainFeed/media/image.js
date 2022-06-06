c29c927f105bfaa02e79ccb3217cedbbd9760f37
Dan Lee <dan-lee1@hotmail.co.uk>
2022-06-06 19:25:24 +0100

    changes one image

--- src/components/mainFeed/media/image.js
+++ src/components/mainFeed/media/image.js
@@ -4,7 +4,9 @@ function ImageMedia(props){
     let [useZoomed, setZoomed] = useState(false);
     let [useImageZoomed, setImageZoomed] = useState(<div></div>);
     let [useImage, setImage] = useState(<div></div>);
-    let [useImageLoc, setImageLoc] = useState(props.image)
+    let [useImageData, setImageData] = useState(props.postData[props.postData.findIndex(function(item, i){
+        return parseInt(item.id) === props.id
+      })])
 
     function makeImgBig(){
         setZoomed(!useZoomed);
@@ -19,22 +21,24 @@ function ImageMedia(props){
           });
         let idAdd = indexOfCurrent + 1
         console.log(indexOfCurrent)
-        setImageLoc(props.postData[indexOfCurrent+1].location)
+        // setImageLoc(props.postData[indexOfCurrent+1].location)
         console.log(props.postData[indexOfCurrent+1])
         console.log(props.postData[indexOfCurrent])
+        setImageData(props.postData[indexOfCurrent+1])
     }
 
 
     useEffect( () => {
         if(props.image !== null){
-            setImage(<div onClick={makeImgBig} className={styles.imgArea}><img className={styles.imgStandard} src={"https://cdn.danlee.uk/content/posts/"+useImageLoc} alt={props.image}/></div>)
+            setImage(<div onClick={makeImgBig} className={styles.imgArea}><img className={styles.imgStandard} src={"https://cdn.danlee.uk/content/posts/"+props.image} alt={props.image}/></div>)
             if(useZoomed === true){
                 document.body.style.overflow = 'hidden'
                 setImageZoomed(<div className={styles.imageZoomedBG}>
                     <span className={styles.date}>{props.date}</span>
                     <span onClick={makeImgBig} className={styles.close}>&times;</span>
-                    <img className={styles.imageZoomed} src={"https://cdn.danlee.uk/content/posts/"+useImageLoc} alt={props.image}/>
+                    <img className={styles.imageZoomed} src={"https://cdn.danlee.uk/content/posts/"+useImageData.location} alt={props.image}/>
                     <span onClick={nextImage} className={styles.nextImage}>Next</span>
+                    <span onClick={nextImage} className={styles.previousImage}>Prev</span>
                     <span className={styles.content}>{props.content}</span>
                     </div>)
             }
@@ -44,7 +48,7 @@ function ImageMedia(props){
             }
         }
 
-    }, [useZoomed])
+    }, [useZoomed, useImageData])
     
     return (
         <div >
