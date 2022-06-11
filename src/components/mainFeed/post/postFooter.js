7a46a76780e49a9ddd8f772f500b5cad36e22429
Dan Lee <dan-lee1@hotmail.co.uk>
2022-06-11 20:18:59 +0100

    seperated download

--- src/components/mainFeed/post/postFooter.js
+++ src/components/mainFeed/post/postFooter.js
@@ -1,34 +1,16 @@
 import { useEffect, useState } from 'react';
-import styles from "./postFooter.module.css"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { saveAs } from "file-saver";
+import Download from '../util/download';
 
 function PostFooter(props){
     const [useDownload, setDownload] = useState()
-    const downloadFile = () => {
-        let name = "https://cdn.danlee.uk/content/posts/"+props.location;
-        console.log(props.location.substring(0,3));
-        if(props.location.substring(0,3) === "QdF"){
-            name = "https://cdn.danlee.uk/original/posts/"+props.location;
-        }
-        saveImg(name);
-    }
-    function saveImg(urlArr){
-        (async () => {
-           let name = props.location;
-           let blob = await fetch(urlArr).then((r) => r.blob());
-           saveAs(blob, name);
-        })();
-    }
-
     useEffect( () => {
         if(props.location !== null){
-            setDownload(<button onClick={downloadFile}><FontAwesomeIcon className={styles.icon} icon={['fas', 'download']}/></button>)
+            setDownload(<Download location={props.location}/>)
         }
     }, [])
 
     return(
-    <div className={styles.footer}>
+    <div style={{padding: '2%'}}>
         {useDownload}
         {/* <button onClick={this.changeHeart} ><FontAwesomeIcon className={styles.icon} icon={this.state.heart}/></button> */}
     </div>
